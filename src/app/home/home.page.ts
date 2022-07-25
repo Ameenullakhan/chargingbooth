e1544e20fafbf23ba3bdfb2b1899a8bb31f52f79
Ameen ulla khan <ameenullakhan313@gmail.com>
2022-07-25 20:19:32 +0530

    added new project..

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+// import { WifiWizard2 } from '@awesome-cordova-plugins/wifi-wizard-2/ngx';
+declare var WifiWizard2: any;
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +8,21 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public wifiSsid = 'Loading...';
+
+  constructor() {
+
+    // console.log(this.wifiWizard2.getConnectedSSID())
+  }
+
+  doGetWifiSsid(){
+    WifiWizard2.getConnectedSSID().then((ssid) => {
+      console.log("Get ssid success: " + ssid);
+      this.wifiSsid = ssid;
+    }).catch((err) => {
+      console.log("Failed to get ssid, please check the error↓");
+      console.error(err);
+    });
+  }
 
 }
